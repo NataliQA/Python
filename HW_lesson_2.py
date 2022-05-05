6e7368eba1afd3a0b95561e60c5949ddb4da55d4
Natalia Bragarnyk <86381206+NataliQA@users.noreply.github.com>
2022-05-05 13:33:20 +0300

    Add files via upload

---
+++ HW_lesson_2.py
@@ -0,0 +1,20 @@
+x = 'Python is cool!'
+string_2 = "test me"
+print(x + "\n",   string_2)
+a = 5
+y = 3
+print(a+y)
+print(a-y)
+print(type(a))
+print(type(string_2))
+u = 5.255
+print(type(u))
+k = [5, 25, 78]
+k.reverse()
+print(k)
+print("Hello" + ",", "world")
+print("Hello", ",", "world")
+t = "who are you?"
+print('I\'m QA')
+print("|".join(t))
+print("|".join(t + x))
